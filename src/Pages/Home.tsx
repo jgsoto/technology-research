48d6410f2561fb72e2ec5389024da0c22d13dddb
jgsoto <sotoj9268@gmail.com>
2026-05-30 16:30:42 -0500

    Create home page implementing useState, useRef, useEffect, and useLayoutEffect

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,25 @@
+import {useState, useEffect, useRef, useLayoutEffect} from 'react';
+
 function Home(){
+    const [count, setCount] = useState(0);
+
+    const boxRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        alert('Home mounted');
+    },[]);
+
+    useLayoutEffect(() => {
+        alert(boxRef.current?.offsetWidth);
+    },[]);
+
     return(
-        <div>
+        <div ref={boxRef}>
             <h1>Homepage</h1>
+
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count +1)}>Increment</button>
+
         </div>
     )
 }
