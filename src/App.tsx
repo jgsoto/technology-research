43def0c71433c8a8b188b9278415e98233383616
MathiasFer <mafernandezc1@uce.edu.ec>
2026-06-01 00:08:31 -0500

    feat(app): connect all atomic design layers in App with ChakraProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import './App.css'
-import { TodoLayout } from './components/templates/TodoLayout'
+import { ChakraProvider } from "@chakra-ui/react";
+import { TodoLayout } from "./components/templates/TodoLayout";
+import { TodoForm } from "./components/molecules/TodoForm";
+import { FilterTabs } from "./components/organisms/FilterTabs";
+import { TodoList } from "./components/organisms/TodoList";
+import { todoStore } from "./store/TodoStore";
 
-function App() {
-  return <TodoLayout />
-}
+export const App = () => {
+  const handleAddTodo = (title: string) => {
+    todoStore.addTodo(title);
+  };
 
-export default App
+  return (
+    <ChakraProvider>
+      <TodoLayout
+        form={<TodoForm onAddTodo={handleAddTodo} />}
+        filters={<FilterTabs />}
+        list={<TodoList />}
+      />
+    </ChakraProvider>
+  );
+};
