c72cc6366f1529fd0770a2d1dfed58c67f9a01ff
MathiasFer <mafernandezc1@uce.edu.ec>
2026-06-01 01:18:30 -0500

    chore: remove legacy todo app files

--- src/components/atoms/InputAtom.tsx
+++
@@ -1,6 +0,0 @@
-import { Input } from "@chakra-ui/react";
-import type React from 'react'
-
-export const InputAtom = (props: React.ComponentProps<typeof Input>) => {
-    return <Input {...props} />
-}
